9020b9d7dd2951cde47b6298f07611c70a98766d
Tigran <tigran.bayburtsyan@gmail.com>
2018-11-15 19:43:37 +0400

    getting tweets based on query and setInterval

---
+++ day7/twitter-interval/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const twit = require('./twitter');
+
+
+const  app = express();
+app.set('view engine', 'ejs');
+const port = 3000;
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.get('/', (req, res) => {
+    res.render('index', {});
+});
+app.post('/s', (req, res) => {
+    const k = req.body.k;
+    twit.search(k, statuses => {
+        res.json({ search: statuses });
+    });
+  });
+
+app.listen(port, () => console.log(`Twitter app listening on port ${port}!`));
